ed3633539eb9b47383763bdecc75352c88b3e5e8
kiku-hub <tacyskani@gmail.com>
2024-10-21 16:09:22 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,8 +8,12 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
-    redirect_to action: :index
+    if Plan.create(plan_params)
+      redirect_to action: :index
+    else
+      flash[:error] = '予定の保存に失敗しました。'
+      redirect_to action: :index
+    end
   end
 
   private
@@ -37,8 +41,21 @@ class CalendarsController < ApplicationController
       end
 
       puts "Date: #{(@todays_date + x)}"
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans }
+      # 現在の日の曜日を取得
+      wday_num = (@todays_date + x).wday
+      
+       # 月、日、曜日、予定を含むハッシュを作成
+       days = { 
+        month: (@todays_date + x).month, 
+        date: (@todays_date + x).day, 
+        wday: wdays[wday_num], 
+        plans: today_plans 
+      }
+
+      # 配列に日を追加
       @week_days.push(days)
     end
   end
